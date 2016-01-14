c4d96c7c4ab67c9a8be6b94304758f05ccc643d0
Jonathan Kurian <kurianj@rose-hulman.edu>
2016-01-13 23:35:48 -0500

    File movement
    This should fix some extraneous files from being added

--- test1/Exam1/src/problem/CmdFrameworkApp.java
+++
@@ -1,9 +0,0 @@
-package problem;
-
-public class CmdFrameworkApp {
-
-	public static void main(String[] args) throws Exception {
-		CmdFramework cmdFramework = new CmdFramework();
-		cmdFramework.execute();
-	}
-}
--- test1/Exam1/src/problem/ICommand.java
+++
@@ -1,9 +0,0 @@
-package problem;
-
-import java.util.List;
-
-public interface ICommand {
-	
-	public String commandExecute(List<String> args);
-
-}
